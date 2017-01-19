827bb19642338874143653e581d6f96cf343bf12
axd465 <axd465@case.edu>
2017-01-19 18:22:27 -0500

    added indicator lines

---
+++ src/testplayer/IndicatorMod.java
@@ -0,0 +1,29 @@
+package testplayer;
+
+import battlecode.common.*;
+
+public class IndicatorMod{
+	// Instance Variables:
+	RobotActor ra;
+	RobotController rc;
+	final int[] ColorCurrentLoc = {0, 255, 0}; // Green
+	final int[] ColorPastLoc = {255, 0, 0}; // Red
+	final int[] ColorDirectionCurrent = {0, 0, 255}; // Blue
+	
+	// Constructor:
+	public IndicatorMod(RobotActor ra_in, RobotController rc_in){
+		ra = ra_in;
+		rc = rc_in;
+	}
+	
+	// Methods:
+	public void drawIndicatorDotLast(){
+		rc.setIndicatorDot(ra.lastLocation, ColorPastLoc[0], ColorPastLoc[1], ColorPastLoc[2]);
+	}
+	public void drawIndicatorDotCurrent(){
+		rc.setIndicatorDot(ra.loc, ColorCurrentLoc[0], ColorCurrentLoc[1], ColorCurrentLoc[2]);
+	}
+	public void drawIndicatorLine(){
+		rc.setIndicatorLine(ra.lastLocation, ra.loc, ColorDirectionCurrent[0], ColorDirectionCurrent[1], ColorDirectionCurrent[2]);
+	}
+}
